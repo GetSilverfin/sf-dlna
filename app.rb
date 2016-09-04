38120b594e99869f3aebc5177b134684db45cf31
Tim Vandecasteele <tim.vandecasteele@gmail.com>
2016-09-04 15:30:42 +0200

    Add a simple overview of the currently rendered screens.

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
+require "sinatra"
 
+URLS = {"http://dashingdemo.herokuapp.com/sample" => {zoom_factor: 4}}
+
+require_relative 'lib/screenshot_refresher'
+screenshot_refresher = ScreenshotRefresher.new(URLS, directory: 'public/screenshots')
+screenshot_refresher.start
 
 get '/' do
-  "Hello world!"
+  @urls_count = URLS.size
+  @latest_screenshots = URLS.map do |url, _options|
+    screenshot_refresher.most_recent_screenshot_for(url).sub('public/', '')
+  end
+  erb :index
 end
